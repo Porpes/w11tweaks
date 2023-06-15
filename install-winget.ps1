f9d0aa66198ac25a5a6b667b4df01292c20e9793
Porpes <74773670+Porpes@users.noreply.github.com>
2023-06-14 23:20:03 -0300

    Update install-winget.ps1

--- install-winget.ps1
+++ install-winget.ps1
@@ -1,65 +1,19 @@
-Function Install-WinGet {
-    #Install the latest package from GitHub
-    [cmdletbinding(SupportsShouldProcess)]
-    [alias("iwg")]
-    [OutputType("None")]
-    [OutputType("Microsoft.Windows.Appx.PackageManager.Commands.AppxPackage")]
-    Param(
-        [Parameter(HelpMessage = "Display the AppxPackage after installation.")]
-        [switch]$Passthru
-    )
-
-    Write-Verbose "[$((Get-Date).TimeofDay)] Starting $($myinvocation.mycommand)"
-
-    if ($PSVersionTable.PSVersion.Major -eq 7) {
-        Write-Warning "This command does not work in PowerShell 7. You must install in Windows PowerShell."
-        return
-    }
-
-    #test for requirement
-    $Requirement = Get-AppPackage "Microsoft.DesktopAppInstaller"
-    if (-Not $requirement) {
-        Write-Verbose "Installing Desktop App Installer requirement"
-        Try {
-            Add-AppxPackage -Path "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -erroraction Stop
-        }
-        Catch {
-            Throw $_
-        }
-    }
-
-    $uri = "https://api.github.com/repos/microsoft/winget-cli/releases"
-
-    Try {
-        Write-Verbose "[$((Get-Date).TimeofDay)] Getting information from $uri"
-        $get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
-
-        Write-Verbose "[$((Get-Date).TimeofDay)] getting latest release"
-        #$data = $get | Select-Object -first 1
-        $data = $get[0].assets | Where-Object name -Match 'msixbundle'
-
-        $appx = $data.browser_download_url
-        #$data.assets[0].browser_download_url
-        Write-Verbose "[$((Get-Date).TimeofDay)] $appx"
-        If ($pscmdlet.ShouldProcess($appx, "Downloading asset")) {
-            $file = Join-Path -path $env:temp -ChildPath $data.name
-
-            Write-Verbose "[$((Get-Date).TimeofDay)] Saving to $file"
-            Invoke-WebRequest -Uri $appx -UseBasicParsing -DisableKeepAlive -OutFile $file
-
-            Write-Verbose "[$((Get-Date).TimeofDay)] Adding Appx Package"
-            Add-AppxPackage -Path $file -ErrorAction Stop
-
-            if ($passthru) {
-                Get-AppxPackage microsoft.desktopAppInstaller
-            }
-        }
-    } #Try
-    Catch {
-        Write-Verbose "[$((Get-Date).TimeofDay)] There was an error."
-        Throw $_
-    }
-    Write-Verbose "[$((Get-Date).TimeofDay)] Ending $($myinvocation.mycommand)"
-}
-
-Install-WinGet;
+# change directory to user downloads
+cd $ENV:UserProfile\Downloads
+# download latest winget installer
+$ProgressPreference = 'SilentlyContinue'; $t = Invoke-WebRequest 'https://api.github.com/repos/microsoft/winget-cli/releases/latest' -UseBasicParsing; $null = ($t.content -Match ',"content_type":"application/octet-stream",.*?"browser_download_url":"(.*\.msixbundle)"'); Invoke-WebRequest $Matches[1] -OutFile .\winget-latest.msixbundle;
+# lookup prerequisites
+$pre = Invoke-WebRequest "https://store.rg-adguard.net/api/GetFiles" -Method POST -Body @{type='CategoryId';url='f855810c-9f77-45ff-a0f5-cd0feaa945c6';ring='Retail';lang='en-US;q=0.6'} -UseBasicParsing
+# download prerequisites
+# Microsoft.UI.Xaml
+$null = ($pre.content -Match '<td><a href="(.*?)" rel="noreferrer">Microsoft\.UI\.Xaml\.2\.7_7\.2207\.21001\.0_x64__8wekyb3d8bbwe\.appx<\/a><\/td>'); Invoke-WebRequest $Matches[1] -OutFile .\Microsoft.UI.Xaml.appx;
+$null = ($pre.content -Match '<td><a href="(.*?)" rel="noreferrer">Microsoft\.VCLibs\.140\.00\.UWPDesktop_14\.0\.30704\.0_x64__8wekyb3d8bbwe\.appx<\/a><\/td>'); Invoke-WebRequest $Matches[1] -OutFile .\Microsoft.VCLibs.appx;
+$null = ($pre.content -Match '<td><a href="(.*?)" rel="noreferrer">Microsoft\.DesktopAppInstaller_2022\.728\.1939\.0_neutral_~_8wekyb3d8bbwe\.msixbundle<\/a><\/td>'); Invoke-WebRequest $Matches[1] -OutFile .\Microsoft.DesktopAppInstaller.msixbundle; $ProgressPreference = 'Continue';
+# install prerequisites
+Add-AppxPackage .\Microsoft.UI.Xaml.appx
+Add-AppxPackage .\Microsoft.VCLibs.appx
+Add-AppxPackage .\Microsoft.DesktopAppInstaller.msixbundle
+# install winget
+Add-AppxPackage .\winget-latest.msixbundle
+# list installed applications
+winget list
